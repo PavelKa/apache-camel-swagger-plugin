c1f690d272ae0e1c378ab5db73056e8d673c84ba
Pavel Kahle <kahle@kahle.cz>
2020-07-27 08:58:04 +0200

    Issues with dependencies resolved

--- src/main/java/cz/kahle/maven/SwaggerGeneratorMojo.java
+++ src/main/java/cz/kahle/maven/SwaggerGeneratorMojo.java
@@ -11,29 +11,18 @@ import org.apache.camel.impl.engine.DefaultClassResolver;
 import org.apache.camel.model.rest.RestsDefinition;
 import org.apache.camel.swagger.RestSwaggerReader;
 import org.apache.commons.io.FileUtils;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.classworlds.realm.ClassRealm;
-import org.springframework.boot.loader.LaunchedURLClassLoader;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
